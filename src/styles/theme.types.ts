667462c1ef6b471ad908d4274ccc0c96f338adaa
Jordan Janzen <jordancjanzen@gmail.com>
2019-01-02 13:45:46 -0800

    Create interfaces for theme

---
+++ src/styles/theme.types.ts
@@ -0,0 +1,43 @@
+export interface IPalette {
+  black: string;
+  white: string;
+  blue: string;
+  red: string;
+  yellow: string;
+  lightblue: string;
+  lightred: string;
+  lightyellow: string;
+}
+
+export interface IDefaultPalette extends IPalette {
+  themePrimary: string;
+  themeDark: string;
+  themeLight: string;
+  themeSecondary: string;
+}
+
+export interface IFonts {
+  fontFamily: string;
+}
+
+export interface ISemanticColors {
+  buttonBackground: string;
+  buttonText: string;
+  linkColor: string;
+  siteBackground: string;
+  textColor: string;
+}
+
+export interface ITheme {
+  fonts: IFonts;
+  isInverted: boolean;
+  palette: IPalette;
+  semanticColors: ISemanticColors;
+}
+
+export interface IPartialTheme {
+  fonts?: Partial<IFonts>;
+  isInverted?: boolean;
+  palette?: Partial<IPalette>;
+  semanticColors?: Partial<ISemanticColors>;
+}
