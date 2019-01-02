0117e51c9844af8aaf601327ab8f08dca1ec9249
Jordan Janzen <jordancjanzen@gmail.com>
2019-01-02 14:48:42 -0800

    Create first styled components

--- src/components/App/App.styles.tsx
+++ src/components/App/App.styles.tsx
@@ -1,2 +1,43 @@
-import React from "react";
 import styled from "styled-components";
+import {getTheme} from '../../styles/theme';
+
+export const Root = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.header`
+  background-color: #222;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  border-top: 5px solid ${getTheme().palette.blue};
+  box-shadow: inset 0 5px ${getTheme().palette.red};
+`;
+
+export const Logo = styled.h1`
+  font-weight: bolder;
+  font-size: 3.5em;
+  margin: .5em 0;
+`;
+
+export const Body = styled.main`
+  padding: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightsList = styled.ul`
+  margin: 0;
+  max-width: 600px;
+  text-align: left;
+`;
+
+export const Right = styled.li`
+
+`
